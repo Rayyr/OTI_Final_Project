daa1369a9937568ba3aede9816564c5b63f969a0
ray <khasaatirayaa@gmail.com>
2025-08-30 11:23:22 +0300

    build usual_siext_b_seq

---
+++ TestBench/UVM_components/sequences/bit_manipulation_sequences/siext_b/usual_siext_b_seq.sv
@@ -0,0 +1,134 @@
+// !! NOTE : in logical ops in general we dont have edge cases so all of them are applicavle without any errors thats why i implement them in one sequence class !
+
+class usual_siext_b_seq extends uvm_sequence #(bmu_sequence_item);
+  
+  //register the class into uvm factory
+  `uvm_object_utils(usual_siext_b_seq)
+ 
+
+
+  //overriden new()
+  function new(string name ="usual_siext_b_seq");
+    super.new(name);
+  endfunction
+  
+  
+  //task stimul
+  task body(); 
+      
+    //declare&initialize sequence_item ( packet , transaction .. )
+    bmu_sequence_item seq=bmu_sequence_item::type_id::create("seq");
+
+
+ //disable the Reset signal for the test of test cases 
+    seq.rst_l.constraint_mode(0);
+    seq.rst_l=1'b1;
+ 
+
+ 
+   seq.ap.costraint_mode(0); 
+   initialize_ap(seq.ap);
+   seq.ap.siext_b=1'b1;
+ 
+ //random input
+   seq.randomize() with { csr_ren_in==1'b0;}; 
+   start_item(seq); 
+   `uvm_info(get_type_name(), ("Standard CPOP random inputs"), UVM_NONE) 
+   finish_item(seq); 
+
+ 
+
+ //a_in=0......0xxxxxx   , result= pos value ( remains pos)
+   seq.randomize() with { csr_ren_in==1'b0;(a_in[31]==1'b0) && (a_in[7]==1'b0) ;};//a[7]&a[31]=0 
+   start_item(seq); 
+   `uvm_info(get_type_name(), ("Standard CPOP with a_in[7]=0 & a_in[31]=0 input"), UVM_NONE) 
+   finish_item(seq);
+
+
+
+
+
+ //a_in=1......1xxxxxx  ,  result= neg value (remains neg)
+   seq.randomize() with { csr_ren_in==1'b0;(a_in[31]==1'b1) && (a_in[7]==1'b1) ;};//a[7]&a[31]=1 
+   start_item(seq); 
+   `uvm_info(get_type_name(), ("Standard CPOP with a_in[7]=1 & a_in[31]=1 input"), UVM_NONE) 
+   finish_item(seq);
+ 
+
+
+
+
+ //a_in=0......1xxxxxx   , result= neg value ( pos to neg )
+   seq.randomize() with { csr_ren_in==1'b0;(a_in[31]==1'b0) && (a_in[7]==1'b1) ;};//a[7]=1 &a[31]=0 
+   start_item(seq); 
+   `uvm_info(get_type_name(), ("Standard CPOP with a_in[7]=1 & a_in[31]=0 input"), UVM_NONE) 
+   finish_item(seq);
+
+
+
+
+ //a_in=1......0xxxxxx   , result= pos value (neg to pos)
+   seq.randomize() with { csr_ren_in==1'b0;(a_in[31]==1'b1) && (a_in[7]==1'b0) ;};//a[7]=0 &a[31]=1 
+   start_item(seq); 
+   `uvm_info(get_type_name(), ("Standard CPOP with a_in[7]=0 & a_in[31]=1 input"), UVM_NONE) 
+   finish_item(seq);
+
+ 
+    
+  endtask
+  
+  
+
+
+
+
+//initilize the ap feilds to 0
+task initialize_ap(output rtl_alu_pkt_t op);
+
+op.clz=0;
+op.ctz=0;
+op.cpop=0;
+op.siext_b=0;
+op.siext_h=0;
+op.min=0;
+op.max=0;
+op.pack=0;
+op.packu=0;
+op.packh=0;
+op.rol=0;
+op.ror=0;
+op.grev=0;
+op.gorc=0;
+op.zbb=0;
+op.bset=0;
+op.bclr=0;
+op.binv=0;
+op.bext=0;
+op.sh1add=0;
+op.sh2add=0;
+op.sh3add=0;
+op.zba=0;
+op.land=0;
+op.lol=0;
+op.lxor=0;
+op.sll=0;
+op.srl=0;
+op.sra=0;
+op.beq=0;
+op.bne=0;
+op.blt=0;
+op.bge=0;
+op.add=0;
+op.sub=0;
+op.slt=0;
+op.unsign=0;
+op.jal=0;
+op.predict_t=0;
+op.predict_nt=0;
+op.csr_write=0;
+op.csr_imm=0;
+
+endtask
+
+endclass
+
