2b0ae2eadda744cb6f0c9db647b348292221b8da
ray <khasaatirayaa@gmail.com>
2025-08-30 13:32:18 +0300

    build usual_pack_seq

---
+++ TestBench/UVM_components/sequences/bit_manipulation_sequences/pack/usual_pack_seq.sv
@@ -0,0 +1,129 @@
+// !! NOTE : in logical ops in general we dont have edge cases so all of them are applicavle without any errors thats why i implement them in one sequence class !
+
+class usual_pack_seq extends uvm_sequence #(bmu_sequence_item);
+  
+  //register the class into uvm factory
+  `uvm_object_utils(usual_pack_seq)
+ 
+
+
+  //overriden new()
+  function new(string name ="usual_pack_seq");
+    super.new(name);
+  endfunction
+  
+  
+  //task stimul
+  task body(); 
+      
+    //declare&initialize sequence_item ( packet , transaction .. )
+    bmu_sequence_item seq=bmu_sequence_item::type_id::create("seq");
+
+
+ //disable the Reset signal for the test of test cases 
+    seq.rst_l.constraint_mode(0);
+    seq.rst_l=1'b1;
+ 
+
+ 
+   seq.ap.costraint_mode(0); 
+   initialize_ap(seq.ap);
+   seq.ap.pack=1'b1;
+ 
+ 
+   seq.randomize() with { csr_ren_in==1'b0;}; 
+   start_item(seq); 
+   `uvm_info(get_type_name(), ("Standard PACK random inputs"), UVM_NONE) 
+   finish_item(seq); 
+
+ 
+
+ //both inputs are zero
+   seq.randomize() with { csr_ren_in==1'b0;a_in==32'h00000000;b_in==32'h00000000;}; 
+   start_item(seq); 
+   `uvm_info(get_type_name(), ("Standard CPOP with 0 inputs"), UVM_NONE) 
+   finish_item(seq);
+
+
+
+
+ //both inputs are 1's bits
+   seq.randomize() with { csr_ren_in==1'b0;a_in==32'hffffffff;b_in==32'hffffffff;}; 
+   start_item(seq); 
+   `uvm_info(get_type_name(), ("Standard CPOP with 1's-bits input"), UVM_NONE) 
+   finish_item(seq);
+ 
+
+
+
+  //a_in=0......  b_in=.....1...   
+   seq.randomize() with { csr_ren_in==1'b0;a_in[31]==1'b0;b_in[15]==1'b1;}; 
+   start_item(seq); 
+   `uvm_info(get_type_name(), ("Standard CPOP with a_in[31]=0  and b_in[15]=1 "), UVM_NONE) 
+   finish_item(seq);
+
+
+
+    //a_in=1......  b_in=.....0...  
+   seq.randomize() with { csr_ren_in==1'b0;a_in[31]==1'b1;b_in[15]==1'b0;}; 
+   start_item(seq); 
+   `uvm_info(get_type_name(), ("Standard CPOP with a_in[31]=1  and b_in[15]= 0 "), UVM_NONE) 
+   finish_item(seq);
+    
+  endtask
+  
+  
+
+
+
+
+//initilize the ap feilds to 0
+task initialize_ap(output rtl_alu_pkt_t op);
+
+op.clz=0;
+op.ctz=0;
+op.cpop=0;
+op.siext_b=0;
+op.siext_h=0;
+op.min=0;
+op.max=0;
+op.pack=0;
+op.packu=0;
+op.packh=0;
+op.rol=0;
+op.ror=0;
+op.grev=0;
+op.gorc=0;
+op.zbb=0;
+op.bset=0;
+op.bclr=0;
+op.binv=0;
+op.bext=0;
+op.sh1add=0;
+op.sh2add=0;
+op.sh3add=0;
+op.zba=0;
+op.land=0;
+op.lol=0;
+op.lxor=0;
+op.sll=0;
+op.srl=0;
+op.sra=0;
+op.beq=0;
+op.bne=0;
+op.blt=0;
+op.bge=0;
+op.add=0;
+op.sub=0;
+op.slt=0;
+op.unsign=0;
+op.jal=0;
+op.predict_t=0;
+op.predict_nt=0;
+op.csr_write=0;
+op.csr_imm=0;
+
+endtask
+
+endclass
+
