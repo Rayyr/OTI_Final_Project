93cfc5f2d3b766af72b4c3ec00f723302a275c89
ray <khasaatirayaa@gmail.com>
2025-09-02 11:09:57 +0300

    final ctz test version , it includes all seqs

--- TestBench/UVM_components/tests/bit_manipulations_tests/ctz_tests.sv
+++ TestBench/UVM_components/tests/bit_manipulations_tests/ctz_tests.sv
@@ -9,6 +9,7 @@ conflict_ctz_data_path_seq seq1;
 conflict_reading_with_ctz_seq seq2;
 reset_seq seq3;
 usual_ctz_seq seq4;
+random_seq seq5;
  
  
 
@@ -27,6 +28,7 @@ usual_ctz_seq seq4;
   
   //run_phase()
   task run_phase(uvm_phase phase);
+ 
     super.run_phase(phase);
     phase.raise_objection(this); 
     
@@ -34,16 +36,17 @@ usual_ctz_seq seq4;
     seq2= conflict_reading_with_ctz_seq::type_id::create("seq2");
     seq3= reset_seq::type_id::create("seq3");
     seq4= usual_ctz_seq::type_id::create("seq4");
+   seq5=random_seq::type_id::create("seq5");
  
 
-    repeat(2) begin 
+   
     
-     seq1.start(bmu_env.agent.sequencer); 
-     seq2.start(bmu_env.agent.sequencer); 
-      seq3.start(bmu_env.agent.sequencer); 
-       seq4.start(bmu_env.agent.sequencer); 
- 
-    end
+    seq1.start(bmu_env.agent.sequencer); 
+    seq2.start(bmu_env.agent.sequencer); 
+    seq3.start(bmu_env.agent.sequencer); 
+    seq4.start(bmu_env.agent.sequencer); 
+    seq5.start(bmu_env.agent.sequencer); 
+
     phase.drop_objection(this); 
     `uvm_info(get_type_name(), "End of testcase", UVM_LOW); 
   endtask
